f7ad6c914e16244c597a6194efae6c14e8ba6dc4
jeeva <jeevasubash64@gmail.com>
2023-12-31 19:30:36 +0530

    API URL changes.

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -3,7 +3,7 @@ import Http from './http';
 
 const http = new Http('https://tags.jee6.in');
 const http_yt = new Http('https://suggestqueries.google.com');
-const http_high = new Http('http://localhost:1314');
+const http_high = new Http('https://www.hightool.net');
 
 export {
     http, http_yt, http_high
